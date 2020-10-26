98bc3870bc64e3cd27b643da04180971859ab6bb
georgetalnagi <gtalnagi@yahoo.com>
2020-10-26 13:54:30 -0400

    commit tree de615bce191965e9bcd6cacdb5ffc59c32dc2ccb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+#Ask if integer is divisible by each number in the range 
+#if range number creates a 0 remainderr when integer is divided by that number, the
+#number is not prime 
+def prime?(integer)
+    start = 2
+    if integer > 1 
+    range = (start..integer-1).to_a
+    #iterate over new numbers 
+    range.none? {|num| integer % num == 0}
+    else false
+    end 
+end 
